b221a77a37f2f8b21b45dc15bd249cbc230b166b
benwxng <benxwang7@gmail.com>
2025-04-16 21:22:21 -0400

    link animations for posts

--- app/components/posts.tsx
+++ app/components/posts.tsx
@@ -1,5 +1,5 @@
-import Link from "next/link";
 import { formatDate, getBlogPosts } from "app/writings/utils";
+import AnimatedLink from "./AnimatedLink";
 
 export function BlogPosts() {
   let allBlogs = getBlogPosts();
@@ -16,20 +16,18 @@ export function BlogPosts() {
           return 1;
         })
         .map((post) => (
-          <Link
-            key={post.slug}
-            className="flex flex-col space-y-1 mb-4"
-            href={`/writings/${post.slug}`}
-          >
+          <div key={post.slug} className="flex flex-col space-y-1 mb-4">
             <div className="w-full flex flex-col md:flex-row space-x-0 md:space-x-2">
-              <p className="text-neutral-600 dark:text-neutral-400 w-[130px] tabular-nums mr-2">
+              <div className="text-neutral-600 dark:text-neutral-400 w-[130px] tabular-nums mr-2">
                 {formatDate(post.metadata.publishedAt, false)}
-              </p>
-              <p className="text-neutral-900 dark:text-neutral-100 tracking-tight">
-                {post.metadata.title}
-              </p>
+              </div>
+              <div className="text-neutral-900 dark:text-neutral-100 tracking-tight">
+                <AnimatedLink href={`/writings/${post.slug}`}>
+                  {post.metadata.title}
+                </AnimatedLink>
+              </div>
             </div>
-          </Link>
+          </div>
         ))}
     </div>
   );
