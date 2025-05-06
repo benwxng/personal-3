38b810bf6d5b4eda6029b75dd818b89a8de4c42f
benwxng <benxwang7@gmail.com>
2025-05-05 23:41:54 -0400

    comment

--- app/components/posts.tsx
+++ app/components/posts.tsx
@@ -16,6 +16,7 @@ export function BlogPosts() {
           return 1;
         })
         .map((post) => (
+          //reconfigure later
           <div key={post.slug} className="flex flex-col space-y-1 mb-4">
             <div className="w-full flex flex-col md:flex-row space-x-0 md:space-x-2">
               <div className="text-neutral-600 dark:text-neutral-400 w-[130px] tabular-nums mr-2">
